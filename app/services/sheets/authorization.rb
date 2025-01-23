054a0a5f9d2da71eaf7ed9f19deee8a715132adc
dave doolin <david.doolin@gmail.com>
2025-01-23 04:52:40 -0800

    more auth stubbing for google sheets

--- app/services/sheets/authorization.rb
+++ app/services/sheets/authorization.rb
@@ -1,5 +1,46 @@
 module Sheets
   class Authorization
-  BASE_AUTH_URL='https://accounts.google.com/o/oauth2/auth'
+    BASE_AUTH_URL="https://accounts.google.com/o/oauth2/auth"
+    BASE_TOKEN_URL="https://oauth2.googleapis.com/token"
+
+    def initialize(client_id, client_secret, redirect_uri)
+      @client_id = client_id
+      @client_secret = client_secret
+      @redirect_uri = redirect_uri
+    end
+
+    def get_auth_url
+      "#{BASE_AUTH_URL}?client_id=#{@client_id}&redirect_uri=#{@redirect_uri}&response_type=code&scope=https://www.googleapis.com/auth/spreadsheets"
+    end
+
+    def get_token(code)
+      response = HTTParty.post(BASE_TOKEN_URL, body: {
+        client_id: @client_id,
+        client_secret: @client_secret,
+        redirect_uri: @redirect_uri,
+        code: code,
+        grant_type: "authorization_code"
+      })
+      JSON.parse(response.body)
+    end
+
+    def refresh_token(refresh_token)
+      response = HTTParty.post(BASE_TOKEN_URL, body: {
+        client_id: @client_id,
+        client_secret: @client_secret,
+        refresh_token: refresh_token,
+        grant_type: "refresh_token"
+      })
+      JSON.parse(response.body)
+    end
+
+    def revoke_token(token)
+      response = HTTParty.post(BASE_TOKEN_URL, body: {
+        token: token,
+        client_id: @client_id,
+        client_secret: @client_secret
+      })
+      JSON.parse(response.body)
+    end
   end
 end
--- spec/services/sheets/authorization_spec.rb
+++ spec/services/sheets/authorization_spec.rb
@@ -1,1 +1,19 @@
+# frozen_string_literal: true
+
 # google sheets auth spec
+
+require 'rails_helper'
+
+describe Sheets::Authorization do
+  let(:client_id) { '1234567890' }
+  let(:client_secret) { '1234567890' }
+  let(:redirect_uri) { 'http://localhost:3000/auth/google/callback' }
+
+  subject { Sheets::Authorization.new(client_id, client_secret, redirect_uri) }
+
+  describe '#get_auth_url' do
+    it 'returns the correct auth url' do
+      expect(subject.get_auth_url).to eq("https://accounts.google.com/o/oauth2/auth?client_id=#{client_id}&redirect_uri=#{redirect_uri}&response_type=code&scope=https://www.googleapis.com/auth/spreadsheets")
+    end
+  end
+end
