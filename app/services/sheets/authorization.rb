984dca235ca629f581ae7b2e238453885bd16788
dave doolin <david.doolin@gmail.com>
2025-01-25 07:19:19 -0800

    tab names from specified google sheet
    The sheet id is in the env file. The code in this
    commit will acquire the names of those tabs.

--- app/services/sheets/authorization.rb
+++ app/services/sheets/authorization.rb
@@ -1,12 +1,65 @@
+
+require "google/apis/sheets_v4"
+require "googleauth"
+require "googleauth/stores/file_token_store"
+
 module Sheets
   class Authorization
     BASE_AUTH_URL="https://accounts.google.com/o/oauth2/auth"
     BASE_TOKEN_URL="https://oauth2.googleapis.com/token"
 
-    def initialize(client_id, client_secret, redirect_uri)
-      @client_id = client_id
-      @client_secret = client_secret
-      @redirect_uri = redirect_uri
+    CREDENTIALS_PATH = Rails.root.join(ENV["GOOGLE_APPLICATION_CREDENTIALS"])
+    CREDENTIALS_JSON = File.read(CREDENTIALS_PATH)
+
+    # The credentials have the following structure:
+    # {
+    #    "type": "service_account",
+    #    "project_id": "xxxx",
+    #    "private_key_id": "xxxx",
+    #    "private_key": "xxx`",
+    #    "client_email": "demoaccount@aerobic-cyclist-440800-v0.iam.gserviceaccount.com",
+    #    "client_id": "xxxx",
+    #    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    #    "token_uri": "https://oauth2.googleapis.com/token",
+    #    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    #    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/demoaccount%40aerobic-cyclist-440800-v0.iam.gserviceaccount.com",
+    #    "universe_domain": "googleapis.com"
+    #  }
+    #
+
+    # We want to initial using the credentials from the .env file.
+    # Note: theee is not "client_secrent" key, and the credentials can be passed as-is.
+    def initialize
+    end
+
+
+    def list_google_sheets(spreadsheet_id, credentials_path)
+      # Initialize the Sheets API client
+      service = Google::Apis::SheetsV4::SheetsService.new
+      service.client_options.application_name = "Google Sheets API Ruby Quickstart"
+
+      # Authorize with credentials.json
+      scopes = [ Google::Apis::SheetsV4::AUTH_SPREADSHEETS_READONLY ]
+      authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+        json_key_io: File.open(credentials_path),
+        scope: scopes
+      )
+
+      service.authorization = authorizer
+
+      # Get spreadsheet details
+      spreadsheet = service.get_spreadsheet(spreadsheet_id)
+
+      # Extract sheet names
+      sheet_names = spreadsheet.sheets.map { |sheet| sheet.properties.title }
+
+      sheet_names
+    end
+
+    # Now use the connection to make a request to the Google Sheets API.
+    # For example, to get the list of spreadsheets.
+    def get_spreadsheets
+      connection.get("/v4/spreadsheets")
     end
 
     def get_auth_url
--- spec/services/sheets/authorization_spec.rb
+++ spec/services/sheets/authorization_spec.rb
@@ -9,11 +9,17 @@ describe Sheets::Authorization do
   let(:client_secret) { '1234567890' }
   let(:redirect_uri) { 'http://localhost:3000/auth/google/callback' }
 
-  subject { Sheets::Authorization.new(client_id, client_secret, redirect_uri) }
+  subject(:authorization) { Sheets::Authorization.new }
 
-  describe '#get_auth_url' do
-    it 'returns the correct auth url' do
-      expect(subject.get_auth_url).to eq("https://accounts.google.com/o/oauth2/auth?client_id=#{client_id}&redirect_uri=#{redirect_uri}&response_type=code&scope=https://www.googleapis.com/auth/spreadsheets")
+  describe '#list_google_sheets' do
+    it 'returns a list of sheet names' do
+      actual = authorization.list_google_sheets(ENV["DEMO_SPREADSHEET_ID"], ENV["GOOGLE_APPLICATION_CREDENTIALS"])
+
+      puts "actual: #{actual}"
+
+      expect(actual).to be_a(Array)
+      expect(actual.length).to be > 0
+      expect(actual.first).to be_a(String)
     end
   end
 end
